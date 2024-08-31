27f7c55f5b9f00f2d7653f0313f086dc4081605c
Micheal Senkasi <MSenkasi@newvision.co.ug>
2024-08-31 21:27:39 +0300

    update-general

--- manager_account_setup.py
+++ manager_account_setup.py
@@ -78,3 +78,33 @@ def account_setup_initiated():
         db.registered_managers.update_one({'username': login_data}, {'$set': update_fields})
         flash("Your account was successfully set", 'success')
         return redirect('/account-setup-page')
+
+@managerAccountSetup.route('/apikey')
+def apikey():
+    db, fs = get_db_and_fs()
+    login_data = session.get('login_username')
+    if login_data is None:
+        flash('Login first', 'error')
+        return redirect('/')
+    else:
+        company = db.registered_managers.find_one({'username': login_data},{'_id':0,'createdAt':0,'code':0,'address':0,'password':0})
+        apikey = db.managers.find_one({'name': company['company_name']})
+        if apikey:
+            api = str(apikey['_id'])
+            if 'apikey_viewed' in apikey:
+                if apikey['apikey_viewed'] == 'yes':
+                    apikey_viewed = 'yes'
+                    first_part = api[:18]
+                    second_part = api[18:]
+                    return render_template('apikey.html', first_part=first_part, second_part=second_part, apikey_viewed = apikey_viewed)
+                else:
+                    apikey_viewed = 'no'
+                    db.managers.update_one({'name': company['company_name']}, {'$set': {'apikey_viewed': 'yes'}})
+                    return render_template('apikey.html',apikey = api, apikey_viewed = apikey_viewed)
+            else:
+                apikey_viewed = 'no'
+                db.managers.update_one({'name': company['company_name']}, {'$set': {'apikey_viewed': 'yes'}})
+                return render_template('apikey.html',apikey = api, apikey_viewed = apikey_viewed)
+        else:
+            flash('No API keys found', 'error')
+            return redirect('/account-setup-page')
